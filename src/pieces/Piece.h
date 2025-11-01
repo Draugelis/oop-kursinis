a5497e6d1887194c22ae0a18382e61d4fd23e77d
Draugelis <viktoras@draugelis.dev>
2025-11-01 15:26:21 +0200

    feat: add abstract piece class
    Signed-off-by: Draugelis <viktoras@draugelis.dev>

---
+++ src/pieces/Piece.h
@@ -0,0 +1,74 @@
+/**
+ * @file Piece.h
+ * @brief Abstract chess piece class
+ *
+ */
+
+#pragma once
+
+#include "bitboards/BitBoard.h"
+#include "core/Color.h"
+#include "core/PieceType.h"
+
+namespace board {
+class BoardState;
+}
+namespace core {
+class Position;
+}
+
+namespace pieces {
+/**
+ * @brief Abstract base class for chess pieces
+ *
+ * Defines the interface and properties for chess pieces including
+ * color, type, and movement tracking. Each specific piece type
+ * must inherit from this class and implement the move generation logic.
+ *
+ * @note This is an ABC (Abstract Base Class)
+ * @see PieceFactory for creating concrete Piece instances
+ */
+class Piece {
+protected:
+  core::Color m_color;
+  core::PieceType m_type;
+  bool m_bHasMoved;
+
+public:
+  /**
+   * @brief Constructs a new chess piece
+   *
+   * @param color core::Color of the piece (WHITE or BLACK)
+   * @param type core::PieceType of the piece (PAWN, KNIGHT, BISHOP, ROOK,
+   * QUEEN, KING)
+   */
+  Piece(core::Color color, core::PieceType type);
+  /**
+   * @brief Virtual destructor
+   */
+  virtual ~Piece() = default;
+
+  // Getters
+  core::Color getColor() const;
+  core::PieceType getType() const;
+  bool hasMoved() const;
+  char getLetter() const;
+
+  // Setter
+  void setMoved();
+
+  /**
+   * @brief Generates a bitboard of all valid move destinations for this piece
+   *
+   * Virtual method that calculates all squares this piece can legally move
+   * to from the given position.
+   *
+   * @param pos Current position of the piece on the board
+   * @param board Current state of the chess board
+   * @return BitBoard Bitboard with bits set for each valid destination square
+   */
+  virtual bitboards::BitBoard
+  getMoveBitBoard(const core::Position &pos,
+                  const board::BoardState &board) const = 0;
+};
+} // namespace pieces
