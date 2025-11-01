c60ec2ab14460b4ca7d9297713cc01e952ab97f6
Draugelis <viktoras@draugelis.dev>
2025-11-01 15:18:56 +0200

    feat: add bitboard header
    Signed-off-by: Draugelis <viktoras@draugelis.dev>

---
+++ src/bitboards/BitBoard.h
@@ -0,0 +1,47 @@
+/**
+ * @file BitBoard.h
+ * @brief Bitboard class implementation
+ *
+ */
+#pragma once
+
+#include "core/Position.h"
+#include <cstdint> // for uint64_t
+#include <vector>
+
+/**
+ * @brief BitBoard representation class
+ *
+ * BitBoard is 64-bit unsigned int where each
+ * bit represents a taken or available position
+ * on a chess board.
+ *
+ * @note Each piece type and color combination has its own bitboard
+ * @see https://www.chessprogramming.org/Bitboards
+ */
+namespace bitboards {
+class BitBoard {
+private:
+  uint64_t m_ullBits;
+
+public:
+  // Good ol' constructors
+  BitBoard() : m_ullBits(0) {};
+  BitBoard(uint64_t value);
+  // Getters
+  bool isSet(const core::Position &pos) const;
+  uint64_t getBits() const;
+  core::Position getFirstSetBit() const;
+  std::vector<core::Position> getSetBitPositions() const;
+  int popCount() const;
+  // Setters
+  void setBit(const core::Position &pos);
+  void clearBit(const core::Position &pos);
+  // Bitwise operator overloads
+  BitBoard operator|(const BitBoard &other) const; // OR
+  BitBoard operator&(const BitBoard &other) const; // AND
+  BitBoard operator~() const;                      // NOT
+  BitBoard operator<<(int shift) const;            // Shift left
+  BitBoard operator>>(int shift) const;            // Shift right
+};
+} // namespace bitboards
