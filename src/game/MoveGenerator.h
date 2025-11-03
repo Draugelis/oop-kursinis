4aad748553a4ec191dafd8cd672e4997f68ccb2b
Draugelis <viktoras@draugelis.dev>
2025-11-04 00:31:55 +0200

    feat: add move generator
    Signed-off-by: Draugelis <viktoras@draugelis.dev>

---
+++ src/game/MoveGenerator.h
@@ -0,0 +1,41 @@
+/**
+ * @file MoveGenerator.h
+ * @brief Pseudo-legal move generator
+ */
+#pragma once
+
+#include "Move.h"
+#include "bitboards/Manager.h"
+#include "core/Position.h"
+#include "game/MoveContext.h"
+#include "pieces/Piece.h"
+#include <vector>
+
+namespace game {
+/**
+ * @brief Generates all possible pseudo-legal moves
+ *
+ * Uses piece's getMoveBitBoard() methods to orchestrate move generation
+ * then converts given bitboard to a Move object vector
+ *
+ * @see MoveValidator is responsible for filtering legal moves
+ */
+class MoveGenerator {
+private:
+  bitboards::Manager &m_bitboards;
+
+  // Convert bitboard destinations to Move objects
+  std::vector<Move> bitboardToMoves(core::Position from, core::PieceType type,
+                                    bitboards::BitBoard destinations);
+
+public:
+  // force direct initialization
+  explicit MoveGenerator(bitboards::Manager &bitboards)
+      : m_bitboards(bitboards) {}
+
+  std::vector<Move> generateMoves(const MoveContext &context);
+  std::vector<Move> generatePieceMoves(core::Position pos,
+                                       const pieces::Piece &piece,
+                                       const MoveContext &context);
+};
+} // namespace game
