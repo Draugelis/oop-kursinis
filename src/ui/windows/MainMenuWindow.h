79947060aaf5900060af416469969d0deff196ca
Draugelis <viktoras@draugelis.dev>
2025-11-15 23:35:30 +0200

    feat: add main menu window header
    Signed-off-by: Draugelis <viktoras@draugelis.dev>

---
+++ src/ui/windows/MainMenuWindow.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QLabel>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
+
+class MainMenuWindow : public QMainWindow {
+  Q_OBJECT;
+
+private:
+  QVBoxLayout *m_pMainLayout;
+  QWidget *m_pCentralWidget;
+
+  QLabel *m_pTitleLabel;
+  QPushButton *m_pStartButton;
+  QPushButton *m_pQuitButton;
+
+  void setupUI();
+  void applyButtonStyle(QPushButton *button);
+
+public:
+  explicit MainMenuWindow(QWidget *parent = nullptr);
+
+signals:
+  void startGameClicked();
+  void quitClicked();
+};
