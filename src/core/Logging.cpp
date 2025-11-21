034a9218bd2dd235cdbb9f7388832568e7383f98
Draugelis <viktoras@draugelis.dev>
2025-11-21 10:11:14 +0200

    feat: add logging categories
    Signed-off-by: Draugelis <viktoras@draugelis.dev>

---
+++ src/core/Logging.cpp
@@ -0,0 +1,5 @@
+#include "Logging.h"
+
+Q_LOGGING_CATEGORY(uiDebug, "ui", QtWarningMsg)
+Q_LOGGING_CATEGORY(gameDebug, "game", QtWarningMsg)
+Q_LOGGING_CATEGORY(resourceDebug, "resources", QtDebugMsg)
---
+++ src/core/Logging.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(uiDebug)
+Q_DECLARE_LOGGING_CATEGORY(gameDebug)
+Q_DECLARE_LOGGING_CATEGORY(resourceDebug)
